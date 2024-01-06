2904d1266326c9cd325a3cd6cf3270d82dd77776
Aloui-Ikram <ikramaloui145@example.com>
2024-01-06 15:56:36 -0800

    first commit

---
+++ tp3.py
@@ -0,0 +1,10 @@
+# from tkinter import * 
+# fentre=Tk()
+# fentre.title("grille wight")
+# for lignes  in range (5):
+#     for colonnes   in range (5):
+#         Button(fentre,text='L%s-C%s' %(lignes,colonnes), borderwidth=1).grid(row=lignes,column=colonnes)
+
+# fentre.mainloop()
+
+    
